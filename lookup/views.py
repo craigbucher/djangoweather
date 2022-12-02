df9dc5abe7aaadeb2f29737b8b24ef7d91d70197
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-12-01 20:41:32 -0500

    html updates

--- lookup/views.py
+++ lookup/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    # pip install requests
+    import requests
+
+    api_request = requests.get(
+        'http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=22302&distance=5&API_KEY=D969EC86-37FB-4820-8E23-82CD2DE0908C')
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = f'Error: {e}'
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
